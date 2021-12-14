6f7e5b470b5a7147b688f0270e8b1aba18091f31
Konner Hartman <konner.hartman@yahoo.com>
2021-12-14 11:33:45 -0600

    added service worker register to index.html, but getting error with addall to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,18 +1,21 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/style.css",
-    "/manifest.webmanifest",
-    "/assets/images/icons/icon-192x192.png",
-    "/assets/images/icons/icon-512x512.png",
-  ];
+  "/",
+  "/index.html",
+  "/style.css",
+  "/db.js",
+  "/index.js",
+  "/manifest.webmanifest",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+];
   
-  const CACHE_NAME = "static-cache-v2";
-  const DATA_CACHE_NAME = "data-cache-v1";
-  
+
   // install
   self.addEventListener("install", function(evt) {
     evt.waitUntil(
+      
       caches.open(CACHE_NAME).then(cache => {
         console.log("Your files were pre-cached successfully!");
         return cache.addAll(FILES_TO_CACHE);
