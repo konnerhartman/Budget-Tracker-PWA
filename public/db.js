820dd8d2b8cc5ed09b8310bc7f283c183c91d9f2
Konner Hartman <konner.hartman@yahoo.com>
2021-12-15 14:08:35 -0600

    added db.js scipt to html. app is now functioning as it should. also added new/old version code back into db

--- public/db.js
+++ public/db.js
@@ -7,6 +7,11 @@ const request = indexedDB.open('budgetDB', 1);
 request.onupgradeneeded = function (e) {
   console.log('Upgrade needed in IndexDB');
 
+  const { oldVersion } = e;
+  const newVersion = e.newVersion || db.version;
+
+  console.log(`DB Updated from version ${oldVersion} to ${newVersion}`);
+
   db = e.target.result;
 
   if (db.objectStoreNames.length === 0) {
